a9836e35a58ef164533134e976edce786d475394
MayarSayed <mayaar.sayed97@gmail.com>
2019-10-29 20:41:21 +0200

    EDIT

--- ScannerGUI/ScannerGUI/Program.cs
+++ ScannerGUI/ScannerGUI/Program.cs
@@ -16,7 +16,7 @@ namespace ScannerGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ScannerApp());
         }
     }
 }
