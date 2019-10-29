9f51bd9bb790e9844c94f823e3e2992898c4c776
MayarSayed <mayaar.sayed97@gmail.com>
2019-10-29 11:05:13 +0200

    GUI

---
+++ ScannerGUI/ScannerGUI/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ScannerGUI
+{
+    partial class ScannerApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputText = new System.Windows.Forms.TextBox();
+            this.GenerateBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputText
+            // 
+            this.InputText.Location = new System.Drawing.Point(33, 63);
+            this.InputText.Multiline = true;
+            this.InputText.Name = "InputText";
+            this.InputText.Size = new System.Drawing.Size(111, 181);
+            this.InputText.TabIndex = 0;
+            this.InputText.TextChanged += new System.EventHandler(this.InputText_TextChanged);
+            // 
+            // GenerateBtn
+            // 
+            this.GenerateBtn.Location = new System.Drawing.Point(51, 250);
+            this.GenerateBtn.Name = "GenerateBtn";
+            this.GenerateBtn.Size = new System.Drawing.Size(75, 23);
+            this.GenerateBtn.TabIndex = 1;
+            this.GenerateBtn.Text = "Generate";
+            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.Click += new System.EventHandler(this.GenerateBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter your Code here";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ScannerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 305);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GenerateBtn);
+            this.Controls.Add(this.InputText);
+            this.Name = "ScannerApp";
+            this.Text = "Scanner App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputText;
+        private System.Windows.Forms.Button GenerateBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ScannerGUI/ScannerGUI/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScannerGUI
+{
+    public partial class ScannerApp : Form
+    {
+        public ScannerApp()
+        {
+            InitializeComponent();
+            output = new List<String>();
+        }
+        //output list of strings
+        public List<string> output;
+        bool my_error = false;
+
+        //function to check letters
+        public bool Is_letter(char a)
+        {
+            if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //fuction to check digits
+        public bool Is_number(char a)
+        {
+            if ((a >= '0' && a <= '9'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //scanner function to scan tokens
+        public void scanner(string s)
+        {
+
+            if (s == "if")
+            {
+                output.Add("if\tIF");
+            }
+            else if (s == "then")
+            {
+                output.Add("then\tTHEN");
+            }
+            else if (s == "read")
+            {
+                output.Add("read\tREAD");
+            }
+            else if (s == "else")
+            {
+                output.Add("else\tELSE");
+            }
+            else if (s == "return")
+            {
+                output.Add("return\tRETURN");
+            }
+            else if (s == "begin")
+            {
+                output.Add("begin\tBEGIN");
+            }
+            else if (s == "end")
+            {
+                output.Add("end\tEND");
+            }
+            else if (s == "main")
+            {
+                output.Add("main\tMAIN");
+            }
+            else if (s == "string")
+            {
+                output.Add("string\tSTRING");
+            }
+            else if (s == "int")
+            {
+                output.Add("int\tINT");
+            }
+            else if (s == "until")
+            {
+                output.Add("until\tUNTIL");
+            }
+            else if (s == "+")
+            {
+                output.Add("+\tPLUS");
+            }
+
+            else if (s == "-")
+            {
+                output.Add("-\tMINUS");
+            }
+            else if (s == "*")
+            {
+                output.Add("*\tMUL");
+            }
+            else if (s == "/")
+            {
+                output.Add("/\tDIVIDE");
+            }
+            else if (s == ";")
+            {
+                output.Add(";\tSEMI");
+            }
+            else if (s == "=")
+            {
+                output.Add("=\tEQUAL");
+            }
+            else if (s == "<=")
+            {
+                output.Add("<=\tLESS THAN OR EQUAL");
+            }
+            else if (s == ">=")
+            {
+                output.Add(">=\tMORE THAN OR EQUAL");
+            }
+            else if (s == ":=")
+            {
+                output.Add(":=\tassign");
+            }
+            else if (s == "[")
+            {
+                output.Add("[\tLeft bracket");
+            }
+            else if (s == "(")
+            {
+                output.Add("(\tLeft bracket");
+            }
+            else if (s == "]")
+            {
+                output.Add("]\tright bracket");
+            }
+            else if (s == ")")
+            {
+                output.Add(")\tright bracket");
+            }
+            else if (s == "<")
+            {
+                output.Add("<\tAngel bracket");
+            }
+            else if (s == ">")
+            {
+                output.Add(">\tAngel bracket");
+            }
+            else if (Is_letter(s[0]))
+            {
+                output.Add(s + "\tidentifier");
+            }
+            else if (Is_number(s[0]))
+            {
+                for (int i = 1; i < s.Length; i++)
+                {
+                    if (!Is_number(s[i]))
+                    {
+                        my_error = true;
+
+                    }
+                }
+                output.Add(s + "\tnumber");
+            }
+            else
+            {
+                my_error = true;
+            }
+
+        }
+
+
+        public string input;
+        public int input_size;
+        public string temp;
+        public int count = 0;
+
+
+        private void InputText_TextChanged(object sender, EventArgs e)
+        {
+            input = InputText.Text;
+            input_size = input.Length;
+        }
+
+        private void GenerateBtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < input_size;)
+            {
+                count = 0;
+                temp = string.Empty;
+                if (Is_letter(input[i]) || Is_number(input[i]))
+                {
+                    temp.resize(temp.length() + 1);
+                    temp[count] = input[i];
+                    count++;
+                    i++;
+                    while ((i < input_size) && (Is_letter(input[i]) || Is_number(input[i])))
+                    {
+                        temp.resize(temp.length() + 1);
+                        temp[count] = input[i];
+                        i++;
+                        count++;
+                    }
+                    scanner(temp);
+                }
+                else if (input[i] == ' ')
+                {
+                    i++;
+                }
+                else if (input[i] == ':' || input[i] == '>' || input[i] == '<')
+                {
+                    temp.resize(temp.Length + 1);
+                    temp[count] = input[i];
+                    i++;
+                    count++;
+                    if (input[i] == '=')
+                    {
+                        temp.resize(temp.Length + 1);
+                        temp[count] = input[i];
+                        i++;
+                        count++;
+                    }
+                    scanner(temp);
+                }
+                else if (input[i] == '+' || input[i] == '-' || input[i] == ';' || input[i] == '*' || input[i] == '/'
+                    || input[i] == '[' || input[i] == ']' || input[i] == '(' || input[i] == ')' || input[i] == '=')
+                {
+                    temp.resize(temp.Length + 1);
+                    temp[count] = input[i];
+                    i++;
+                    count++;
+                    scanner(temp);
+                }
+                else if (input[i] == '{')
+                {
+
+                    i++;
+                    while (input[i] != '}' && i < input_size)
+                    {
+                        i++;
+                    }
+                    i++;
+                }
+                else
+                {
+                    my_error = true;
+                    break;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
